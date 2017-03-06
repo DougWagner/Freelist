ad6e99b6653ef1b9ad3ad25fb5ee686b622b22ff
DougWagner <digwagner@gmail.com>
2017-03-05 18:24:36 -0800

    Cleaned up main.c Awaiting the real tests now

--- src/main.c
+++ src/main.c
@@ -8,33 +8,6 @@
 
 int main() {
     fl_install_heap( HEAP_SIZE );
-    size_t allocations[10] = { 20, 50, 400, 80, 128, 256, 88, 4096, 1181, 512 };
-    void * ptrs[10];
-    for ( size_t i = 0; i < 10; i++ ) {
-        ptrs[i] = fl_malloc_p( allocations[i] );
-    }
-
-    fl_debug_print();
-    fl_free( ptrs[7] );
-    fl_debug_print();
-    fl_free( ptrs[0] );
-    fl_debug_print();
-    fl_free( ptrs[4] );
-    fl_debug_print();
-    fl_free( ptrs[5] );
-    fl_debug_print();
-    fl_free( ptrs[6] );
-    fl_debug_print();
-
-    void * newptr1 = fl_malloc_p( 200 );
-    fl_debug_print();
-    void * newptr2 = fl_malloc_p( 128 );
-    fl_debug_print();
-    void * newptr3 = fl_malloc_p( 10 );
-    fl_debug_print();
-    void * newptr4 = fl_malloc_p( 10 );
-    fl_debug_print();
-
     run_tests();
     fl_uninstall_heap();
     return 0;
